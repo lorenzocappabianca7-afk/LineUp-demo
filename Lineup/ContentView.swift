e03108417a4ad5610e636ee991bc7cc6b0b28a7c
Lorenzo <lorenzo.cappabianca7@gmail.com>
2026-02-07 14:48:28 +0100

    Initial Commit

---
+++ Lineup/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lineup
+//
+//  Created by Lorenzo Cappabianca on 07/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lineup/LineupApp.swift
@@ -0,0 +1,17 @@
+//
+//  LineupApp.swift
+//  Lineup
+//
+//  Created by Lorenzo Cappabianca on 07/02/26.
+//
+
+import SwiftUI
+
+@main
+struct LineupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
